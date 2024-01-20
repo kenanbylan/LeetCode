61c6368d8638f351156502618fe4b16825410f73
Kenan Baylan <76161957+kenanbylan@users.noreply.github.com>
2024-01-20 17:39:21 +0300

    Time: 15 ms (18.38%), Space: 15.9 MB (5.59%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.swift
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.swift
@@ -1,28 +1,41 @@
 class Solution {
     func mergeAlternately(_ word1: String, _ word2: String) -> String {
-            var merged = ""
-            var i = 0
-            var j = 0
+        
+        
+        var mergedString: String = ""
+        var i = 0 
+        var j = 0
+        
+        
+        // It is first added to mergeString one by one sequentially.
+        
+        while i < word1.count && j < word2.count {
+            let index1 = word1.index(word1.startIndex,offsetBy:i)
+            mergedString.append(word1[index1])
+            i += 1
+            
+            let index2 = word2.index(word2.startIndex,offsetBy:j)
+            mergedString.append(word2[index2])
+            j += 1    
+        }
 
-            while i < word1.count && j < word2.count {
-                merged.append(word1[word1.index(word1.startIndex, offsetBy: i)])
-                i += 1
-                merged.append(word2[word2.index(word2.startIndex, offsetBy: j)])
-                j += 1
-            }
-
-            while i < word1.count {
-                merged.append(word1[word1.index(word1.startIndex, offsetBy: i)])
-                i += 1
-            }
-
-            while j < word2.count {
-                merged.append(word2[word2.index(word2.startIndex, offsetBy: j)])
-                j += 1
-            }
-
-            return merged
-
-            }
+        
+        // then if word1 or word2 is not equal, it is added to the end of the mergeString
+        
+        while i < word1.count {
+            let index1 = word1.index(word1.startIndex,offsetBy:i)
+            mergedString.append(word1[index1])
+            i += 1   
+        }
+        
+        while j < word2.count {
+            let index2 = word2.index(word2.startIndex,offsetBy:j)
+            mergedString.append(word2[index2])
+            j += 1   
+        }
+        
+        return mergedString
+    }
 }
 
+// Kenan Baylan 
