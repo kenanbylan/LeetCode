8a8f3f1b4c6a2fa01852f0661f227d1a4185636f
Kenan Baylan <76161957+kenanbylan@users.noreply.github.com>
2024-01-31 22:45:50 +0300

    Time: 8 ms (23.77%), Space: 15.4 MB (54.99%) - LeetHub

---
+++ 00nr-sentence-classnr-s028-nr-sentencenr-sentence-classnr-s1-nr-sentence-nr-sentence-classnr-s2find-the-index-of-the-first-occurrence-in-a-string-nr-sentence/00nr-sentence-classnr-s028-nr-sentencenr-sentence-classnr-s1-nr-sentence-nr-sentence-classnr-s2find-the-index-of-the-first-occurrence-in-a-string-nr-sentence.swift
@@ -0,0 +1,22 @@
+class Solution {
+    func strStr(_ haystack: String, _ needle: String) -> Int {
+        
+        let hayStackCount: Int = haystack.count 
+        let needleCount: Int = needle.count 
+
+        if haystack.isEmpty || hayStackCount < needleCount {
+            return -1       // MARK: needle not found.
+        }
+        
+        for i in 0...(hayStackCount - needleCount) {
+            let firstIndex = haystack.index(haystack.startIndex, offsetBy: i)
+            let lastIndex = haystack.index(firstIndex, offsetBy: needleCount)
+            let newString = haystack[firstIndex..<lastIndex]
+            
+            if newString == needle {
+                return i //index found.
+            }
+        }
+        return -1
+    }
+}
